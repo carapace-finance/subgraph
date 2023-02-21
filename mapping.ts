fd631499a39b91280b26089c80a602f2c3b00d35
vnadoda <vipul@carapace.finance>
2023-02-21 11:07:44 -0600

    Fixed subgraph schema and mapping
    - removed build dir from repo

--- mapping.ts
+++ mapping.ts
@@ -5,8 +5,7 @@ export function handleProtectionPoolInitialized(
   event: ProtectionPoolInitialized
 ): void {
   let protectionPool = new ProtectionPool(event.transaction.from.toHex());
-  protectionPool.underlyingToken = event.params.underlyingToken.toHexString();
-  protectionPool.referenceLendingPools =
-    event.params.referenceLendingPools.toHexString();
+  protectionPool.underlyingToken = event.params.underlyingToken;
+  protectionPool.referenceLendingPools = event.params.referenceLendingPools;
   protectionPool.save();
 }
